68223ae70e978d5774eb1055640ad47d0f9040a9
Salla Reznov <salladiagne14@gmail.com>
2024-04-05 11:00:41 +0200

    start working on animations

--- src/main/scala/snake/Main.scala
+++ src/main/scala/snake/Main.scala
@@ -1,33 +1,72 @@
 package snake
 
+import scalafx.animation.KeyFrame
+import scalafx.animation.Timeline
+import scalafx.animation.Timeline.*
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.beans.property.ObjectProperty
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color.*
 import scalafx.scene.shape.Rectangle
+import scalafx.util.Duration
+import scalafx.beans.property.IntegerProperty
+import javafx.scene.input.KeyCode
 
 object Main extends JFXApp3 {
 
+  // stage (scene)
+  // timeline on stage
+  // timeline onFinished => update snakeState => onChange => redraw snake
+
+  // direction:
+  // 1 = haut
+  // 2 = bas
+  // 3 = gauche
+  // 4 = droite
+
   override def start(): Unit = {
-    val snake = Snake(List((250, 200), (225, 200), (200, 200)))
+    val snakeState: ObjectProperty[Snake] = ObjectProperty(Snake(List((250, 200), (225, 200), (200, 200))))
+    val direction                         = IntegerProperty(4) // droite
 
     stage = new PrimaryStage {
-        title = "Snake"
-        width = 600
-        height = 600
-        scene = new Scene {
-            fill = White
-            content = snake.body.map { (cellX, cellY) =>
-                new Rectangle {
-                    x = cellX
-                    y = cellY
-                    width = 25
-                    height = 25
-                    fill = Green
-                }
-            }
+      title = "Snake"
+      width = 600
+      height = 600
+      scene = new Scene {
+        fill = White
+        content = drawSnake(snakeState)
+        snakeState.onChange {
+          content = drawSnake(snakeState)
         }
+        onKeyPressed = { key =>
+          key.getCode() match {
+            case KeyCode.UP    => direction.update(1)
+            case KeyCode.DOWN  => direction.update(2)
+            case KeyCode.LEFT  => direction.update(3)
+            case KeyCode.RIGHT => direction.update(4)
+            case _             => ()
+          }
+        }
+      }
     }
+
+    new Timeline {
+      keyFrames = List(KeyFrame(time = Duration(25), onFinished = _ => snakeState.update(snakeState.value)))
+      cycleCount = Indefinite
+    }.play()
+
   }
-  
+
+  def drawSnake(snakeState: ObjectProperty[Snake]) =
+    snakeState.value.body.map { (cellX, cellY) =>
+      new Rectangle {
+        x = cellX
+        y = cellY
+        width = 25
+        height = 25
+        fill = Green
+      }
+    }
+
 }
